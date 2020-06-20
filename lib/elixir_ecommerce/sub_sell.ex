6ee58d3c085ff3b28686e9b83827e8d70047fc75
Alan Borges <sr.alan.bispo@gmail.com>
2020-06-19 22:50:03 -0300

    SubSell testing

--- lib/elixir_ecommerce/sub_sell.ex
+++ lib/elixir_ecommerce/sub_sell.ex
@@ -23,4 +23,13 @@ defmodule ElixirEcommerce.SubSell do
     |> cast(attrs, [:amount_sold, :status])
     |> validate_required([:amount_sold, :status])
   end
+
+  def create(attrs \\ %{}) do
+    %SubSell{}
+    |> cast(attrs, [:amount_sold, :status])
+    |> put_assoc(:sell, attrs[:sell])
+    |> put_assoc(:product, attrs[:product])
+    |> put_assoc(:shipping, attrs[:shipping])
+    |> validate_required([:amount_sold, :status, :sell, :product, :shipping])
+  end
 end
---
+++ test/elixir_ecommerce/sub_sell_test.ex
@@ -0,0 +1,28 @@
+defmodule ElixirEcommerce.SellTest do
+  use ElixirEcommerce.DataCase
+  alias ElixirEcommerce.{
+    Department,
+    Product,
+    Sell,
+    ProductTest
+  }
+
+  def sub_sell_fixture do
+  end
+
+  describe "SubSell" do
+    test "should be created with valid params" do
+      assert {:ok, product} =
+        @valid_attrs_first
+        |> Map.put(:department, department)
+        |> Product.create()
+      assert product.department.name == department.name
+    end
+
+    test "should not be created with invalid params" do
+    end
+
+    test "should be updated with valid params" do
+    end
+  end
+end
