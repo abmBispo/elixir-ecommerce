2b942eaff2948226569b6b7c9067af13738fb6d9
Alan Borges <sr.alan.bispo@gmail.com>
2020-06-22 14:00:53 -0300

    SubSell testing finished

--- lib/elixir_ecommerce/sub_sell.ex
+++ lib/elixir_ecommerce/sub_sell.ex
@@ -23,18 +23,45 @@ defmodule ElixirEcommerce.SubSell do
 
   @doc false
   def changeset(sub_sell, attrs) do
-    sub_sell
-    |> cast(attrs, [:amount_sold, :status])
-    |> validate_inclusion(:status, @status_enum)
-    |> put_change(:status, List.first(@status_enum))
-    |> put_assoc(:sell, attrs[:sell])
-    |> put_assoc(:product, attrs[:product])
-    |> validate_required([:amount_sold, :status, :sell, :product])
+    if is_nil(attrs[:status]) do
+      sub_sell
+      |> cast(attrs, [:amount_sold, :status])
+      |> validate_inclusion(:status, @status_enum)
+      |> put_change(:status, List.first(@status_enum))
+      |> put_assoc(:sell, attrs[:sell])
+      |> put_assoc(:product, attrs[:product])
+      |> validate_number(:amount_sold, greater_than: 0)
+      |> validate_required([:amount_sold, :status, :sell, :product])
+    else
+      sub_sell
+      |> cast(attrs, [:status])
+      |> validate_inclusion(:status, @status_enum)
+      |> validate_number(:amount_sold, greater_than: 0)
+      |> validate_required([:amount_sold, :status, :sell, :product])
+    end
   end
 
   def create(attrs \\ %{}) do
     %SubSell{}
     |> SubSell.changeset(attrs)
     |> Repo.insert()
+  end
+
+  def retrieve(id) when is_integer(id) do
+    SubSell
+    |> Repo.get(id)
+    |> Repo.preload(sell: :client)
+    |> Repo.preload(product: :department)
+    |> Repo.preload(:shipping)
+  end
+
+  def update(%SubSell{} = sub_sell, attrs \\ %{}) do
+    sub_sell
+    |> SubSell.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete(%SubSell{} = sub_sell) do
+    Repo.delete(sub_sell)
   end
 end
--- test/elixir_ecommerce/sub_sell_test.exs
+++ test/elixir_ecommerce/sub_sell_test.exs
@@ -13,8 +13,21 @@ defmodule ElixirEcommerce.SubSellTest do
   @product_attrs %{name: "NBA shorts", amount: 100, price: 1999}
   @department_attrs %{name: "Sports"}
   @valid_attrs %{amount_sold: 5}
+  @invalid_attrs %{amount_sold: 0}
+  @valid_update_attrs %{status: "accepted"}
+  @invalid_update_attrs %{status: "not_valid_state"}
 
   def sub_sell_fixture do
+    sell = sell_fixture()
+    product = product_fixture()
+
+    assert {:ok, sub_sell} =
+      @valid_attrs
+      |> Map.put(:sell, sell)
+      |> Map.put(:product, product)
+      |> SubSell.create
+
+    sub_sell
   end
 
   def user_fixture(attrs \\ %{}) do
@@ -58,6 +71,59 @@ defmodule ElixirEcommerce.SubSellTest do
         |> Map.put(:sell, sell)
         |> Map.put(:product, product)
         |> SubSell.create
+    end
+
+    test "should not be created with invalid params" do
+      sell = sell_fixture()
+      product = product_fixture()
+
+      assert {:error, _} =
+        @invalid_attrs
+        |> Map.put(:sell, sell)
+        |> Map.put(:product, product)
+        |> SubSell.create
+    end
+
+    test "should not be created without referenced sell" do
+      product = product_fixture()
+
+      assert {:error, _} =
+        @valid_attrs
+        |> Map.put(:product, product)
+        |> SubSell.create
+    end
+
+    test "should not be created without referenced product" do
+      product = product_fixture()
+
+      assert {:error, _} =
+        @invalid_attrs
+        |> Map.put(:product, product)
+        |> SubSell.create
+    end
+
+    test "should be retrieved" do
+      created_sub_sell =
+        sub_sell_fixture()
+        |> Repo.preload([:sell, :product, :shipping])
+
+      assert created_sub_sell == SubSell.retrieve(created_sub_sell.id)
+    end
+
+    test "should be updated with valid params" do
+      created_sub_sell = sub_sell_fixture()
+      assert {:ok, updated_sub_sell} = SubSell.update(created_sub_sell, @valid_update_attrs)
+      assert updated_sub_sell.status == "accepted"
+    end
+
+    test "should not be updated with invalid params" do
+      created_sub_sell = sub_sell_fixture()
+      assert {:error, _} = SubSell.update(created_sub_sell, @invalid_update_attrs)
+    end
+
+    test "should be deleted" do
+      created_sub_sell = sub_sell_fixture()
+      assert {:ok, _} = SubSell.delete(created_sub_sell)
     end
   end
 end
