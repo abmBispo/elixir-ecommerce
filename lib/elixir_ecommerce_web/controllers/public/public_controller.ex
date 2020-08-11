64a4a8d49ffe448c3b309ce7ddafaec6df580e5d
Alan Borges <sr.alan.bispo@gmail.com>
2020-08-10 21:26:19 -0300

    Index redesign

--- lib/elixir_ecommerce_web/controllers/public/public_controller.ex
+++ lib/elixir_ecommerce_web/controllers/public/public_controller.ex
@@ -13,6 +13,14 @@ defmodule ElixirEcommerceWeb.PublicController do
     departments = Department.all()
     user = Guardian.Plug.current_resource(conn)
 
+    sport_department =
+      Department.retrieve(%{ name: "Sports" })
+      |> List.first()
+
+    games_department =
+      Department.retrieve(%{ name: "Games" })
+      |> List.first()
+
     products = if params["department"] do
       Product.retrieve(department_id: params["department"])
       |> Repo.paginate(page: params["page"], page_size: 25)
@@ -20,6 +28,11 @@ defmodule ElixirEcommerceWeb.PublicController do
       Product.all()
     end
 
-    render(conn, "index.html", products: products, departments: departments, current_user: user)
+    render(conn, "index.html",
+      products: products,
+      current_user: user,
+      departments: departments,
+      games_department: games_department,
+      sport_department: sport_department)
   end
 end
